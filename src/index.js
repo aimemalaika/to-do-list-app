1501fd8193edd4c9114ebaf867af4631135f729b
Aime Malaika <aimemalaika1995@gmail.com>
2022-02-15 00:24:06 +0200

    Added delete and add function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import TodoContainer from './TodoContainer';
 import reportWebVitals from './reportWebVitals';
 
